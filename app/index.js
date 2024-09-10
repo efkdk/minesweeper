d8a9b87be307a9a6cc1aa512370e126cc1c1cbd0
efkdk <dovbikivan2@gmail.com>
2024-09-10 18:50:55 +0300

    feat: add basic functional of minesweeper

---
+++ app/index.js
@@ -0,0 +1,212 @@
+const root = document.getElementById("root");
+const gameStatus = document.getElementById("game-status");
+const restartButton = document.getElementById("restart");
+const flagsCount = document.getElementById("flags-count");
+
+// configuration
+let width = 9,
+  height = 9,
+  bombs = 10,
+  flags = bombs,
+  size = 34;
+
+flagsCount.textContent = flags;
+root.style.gridTemplateColumns = `repeat(${width}, ${size}px)`;
+
+class Cell {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.bomb = false;
+    this.flagged = false;
+    this.open = false;
+  }
+}
+
+Cell.prototype.draw = function (field) {
+  console.log("draw");
+  flagsCount.textContent = flags;
+  checkGameWin();
+  const cellElement = document.querySelector(
+    `[data-x="${this.x}"][data-y="${this.y}"]`
+  );
+  let bombsAround = this.cellsAround(field).filter((cell) => cell.bomb).length;
+  cellElement.classList.value = `cell size-${size}`;
+  if (this.flagged) {
+    cellElement.classList.add("flagged");
+  } else if (!this.open) {
+    cellElement.classList.add("closed");
+  } else if (this.open) {
+    cellElement.classList.add("opened");
+    if (this.bomb) {
+      cellElement.classList.add("type10");
+    } else {
+      cellElement.classList.add(`type${bombsAround}`);
+      if (bombsAround > 0) {
+        cellElement.textContent = bombsAround;
+      }
+    }
+  }
+};
+
+Cell.prototype.cellsAround = function (field) {
+  let cells = [];
+
+  for (let y = this.y - 1; y <= this.y + 1; y++) {
+    for (let x = this.x - 1; x <= this.x + 1; x++) {
+      if (x === this.x && y === this.y) continue;
+      if (x >= 0 && x < width && y >= 0 && y < height) {
+        cells.push(field[y][x]);
+      }
+    }
+  }
+
+  return cells;
+};
+
+Cell.prototype.click = function (field) {
+  if (this.open) {
+    return;
+  } else if (this.flagged) {
+    return;
+  } else if (this.bomb) {
+    this.open = true;
+    this.draw(field);
+    gameOver();
+  } else {
+    this.open = true;
+    let cells = this.cellsAround(field);
+    if (cells.filter((cell) => cell.bomb).length === 0) {
+      cells.forEach((cell) => {
+        cell.click(field);
+      });
+    }
+    this.draw(field);
+  }
+};
+
+Cell.prototype.flag = function () {
+  if (this.open) {
+    return;
+  } else {
+    this.flagged = !this.flagged;
+    this.flagged ? flags-- : flags++;
+    this.draw(field);
+  }
+};
+
+const eachCell = (callback) => {
+  field.forEach((row) => {
+    row.forEach((cell) => {
+      callback(cell);
+    });
+  });
+};
+
+const init = () => {
+  let field = [];
+  for (let y = 0; y < height; y++) {
+    let row = [];
+    for (let x = 0; x < width; x++) {
+      row.push(new Cell(x, y));
+    }
+    field.push(row);
+  }
+  for (let i = 0; i < bombs; i++) {
+    while (true) {
+      let x = Math.floor(Math.random() * width);
+      let y = Math.floor(Math.random() * height);
+      if (!field[y][x].bomb) {
+        field[y][x].bomb = true;
+        break;
+      }
+    }
+  }
+  return field;
+};
+
+let field = init();
+
+const render = () => {
+  eachCell((cell) => {
+    let cellElement = document.createElement("div");
+    cellElement.classList.add("cell", "closed", `size-${size}`);
+    cellElement.dataset.x = cell.x;
+    cellElement.dataset.y = cell.y;
+    root.append(cellElement);
+  });
+};
+
+render();
+
+const openAll = () => {
+  eachCell((cell) => {
+    cell.open = true;
+  });
+  draw();
+};
+
+function draw() {
+  eachCell((cell) => {
+    cell.draw(field);
+  });
+}
+const gameOver = () => {
+  gameStatus.textContent = "You lose :(";
+  openAll();
+};
+
+const checkGameWin = () => {
+  let openCells = 0;
+  eachCell((cell) => {
+    if (cell.open) {
+      openCells++;
+    }
+  });
+  if (openCells === width * height - bombs) {
+    gameStatus.textContent = "You win :)";
+    openAll();
+  }
+};
+
+const restart = () => {
+  root.innerHTML = "";
+  gameStatus.textContent = "";
+  field = init();
+  render();
+  flags = bombs;
+  flagsCount.textContent = flags;
+};
+
+document.addEventListener("click", (event) => {
+  if (event.target.classList.contains("cell")) {
+    const x = +event.target.dataset.x,
+      y = +event.target.dataset.y;
+    field[y][x].click(field);
+
+    let cells = field[y][x].cellsAround(field);
+    let bombsAround = cells.filter((cell) => cell.bomb).length;
+    let flagsAround = cells.filter((cell) => cell.flagged).length;
+    if (bombsAround === flagsAround && bombsAround > 0) {
+      cells.forEach((cell) => {
+        if (!cell.flagged) {
+          cell.click(field);
+        }
+      });
+    }
+  }
+  if (event.target.id === "restart") {
+    restart();
+  }
+});
+
+document.addEventListener("contextmenu", (event) => {
+  event.preventDefault();
+  if (event.target.classList.contains("cell")) {
+    const x = +event.target.dataset.x,
+      y = +event.target.dataset.y;
+    field[y][x].flag();
+  }
+});
+
+console.log(field);
